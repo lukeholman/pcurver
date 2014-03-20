6495a959587823e5c9dacf0f9de287f7e98458f2
lukeholman <lukeholman@gmail.com>
2014-03-20 11:51:20 +1100

    Update lukes_skew_code.R

--- lukes_skew_code.R
+++ lukes_skew_code.R
@@ -1,4 +1,3 @@
-
 luke.skew.test <- function(p.values, reps = 100000)
 {
   p.values <- p.values[which(p.values > 0.03 & p.values < 0.05)]
@@ -23,7 +22,100 @@ luke.skew.test <- function(p.values, reps = 100000)
   # To do: add a histogram of the boot values with a little arrow on it showing the observed skew level, like this: http://stackoverflow.com/questions/11122002/how-do-i-draw-an-arrow-on-a-histogram-drawn-using-ggplot2
 }
 
+# New stuff added on 20 March. Sorry about the mess! 
 
+\name{luke.skew.test2}
+\alias{luke.skew.test2}
+\title{Test whether p values in the range 0.03-0.05 are clustered near 0.05.}
+\usage{
+  luke.skew.test2(p, reps)
+}
+\arguments{
+  \item{p}{A vector of p values. Only those in the range 0.03-0.05 inclusive will be used.}
+  \item{reps}{Number of replicates to use when generating the null distribution. Defaults to 10,000.}
+  \item{quantiles}{A vector of length 2: quantiles to be returned for the null distribution. Defaults to 0.025 and 0.975, i.e. 95% of the data.}
+}
+\value{
+  A dataframe giving the observed and expected mean and median of the p values, as well as the specified quantiles of the latter. The respective one-tailed p values are also given.
+}
+\description{
+  This function tests whether the mean and the median are significantly higher than predicted under the null hypothesis that the true average effect size underlying the p values in the sample is zero, and there is no p-hacking (sensu Simonsohn et al 2013). Under the this null model, the distribution of p values from 0-1 should be flat, and hence the mean and median of the p values in the range 0.03-0.05 should be 0.04. The function computes the expected distribution of means and medians under the null hypothesis, and calculates a one-tailed p that the observed mean and median are higher than expected by chance (indicating p hacking).
+}
+\examples{
+  p <- c(0.00001, 0.024, 0.002, 0.045, 0.00003, 0.021, 0.0001, 0.0000948, 0.0000002)
+  luke.skew.test2(p)
+}
+\references{
+  Simonsohn, Uri and Nelson, Leif D. and Simmons, Joseph P.,
+  P-Curve: A Key to the File Drawer (April 24, 2013). Journal
+  of Experimental Psychology: General, Forthcoming. Available
+  at SSRN: http://ssrn.com/abstract=2256237
+}
+\seealso{
+  \code{\link{binomial.bias.test}},
+  \code{\link{binomial.sns.test}}
+}
+\keyword{curve}
+\keyword{p}
+
+luke.skew.test2 <- function(p, reps = 10000, quantiles = c(0.025, 0.975))
+{
+    n <- length(p)
+  
+    if(n == 0) 
+      {
+          print("Argument 'p' has length of zero; enter multiple p values for best results.")
+          return(NA)
+      }
+    
+    if(reps == 0) 
+      {
+          print("Argument 'reps' is zero; must be 1 or greater (numbers 10,000 and above give good accuracy).")
+          return(NA)
+      }
+    
+    if(length(reps) != 1) 
+    {
+      print("Argument 'reps' must have a length of 1.")
+            return(NA)
+    }
+    
+    if(length(quantiles) != 2) 
+    {
+      print("Argument 'quantiles' must have a length of 2.")
+            return(NA)
+    }
+    
+    if(min(quantiles) <0 || max(quantiles) > 1) 
+    {
+      print("Argument 'quantiles' must be a vector of length 2, in the range 0-1.")
+      return(NA)
+    }
+    
+    p <- p[which(p >= 0.03 & p <= 0.05)]
+  
+    observed.mean <- mean(p) # Should be 0.04 or lower under the null hypothesis of no p-hacking
+    observed.median <- median(p) # Should be 0.04 or lower under the null hypothesis of no p-hacking
+    
+    # Now find distribution of the expected means and medians, given sampling error with n samples
+    rand <- matrix(runif(reps*n, min = 0.03, max = 0.05), ncol = n, nrow = reps)
+    means <- apply(rand, 1, mean) #luke.skew.test2.means
+    medians <- apply(rand, 1, median) #luke.skew.test2.medians
+    
+    # Find the requested quantiles for the null distribution of means and medians
+    mean.quantiles <- quantile(means, probs = quantiles)
+    median.quantiles <- quantile(medians, probs = quantiles)
+    
+    # One-tailed p value
+    p.null.mean <- length(means[means > observed.mean]) / reps
+    p.null.median <- length(medians[means > observed.median]) / reps
+    
+    # Write the output dataframe
+    output <- data.frame(observed = c(observed.mean,observed.median), expected = c(0.04,0.04), lowerCIs = c(mean.quantiles[1],median.quantiles[1]), upperCIs =  c(mean.quantiles[2],median.quantiles[2]), one.tailed.p = c(p.null.mean,p.null.median))
+    rownames(output) <- c("Mean", "Median")
+    return(output)
+}
+##### End of new stuff
 
 We can use a similar approach to compare the skew of two distributions. Basically resample the shit out of the two sets of p value, find the difference in skew each time, and then see if the 95% CIs of the distribution of bootstrapped differences includes zero:
 
